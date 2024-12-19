fc4ddee8773432947372d2903f8f859e4173f252
oop778 <oskardhavel@gmail.com>
2024-12-19 23:40:45 +0200

    Remove AtomComponent & EventBusRegistry

--- src/main/java/net/endergrid/atom/AtomComponent.java
+++
@@ -1,4 +0,0 @@
-package net.endergrid.atom;
-
-public interface AtomComponent {
-}
--- src/main/java/net/endergrid/atom/event/bus/AtomEventBusRegistry.java
+++
@@ -1,9 +0,0 @@
-package net.endergrid.atom.event.bus;
-
-import dev.oop778.bindings.type.Bindable;
-import lombok.NonNull;
-import org.jetbrains.annotations.Nullable;
-
-public interface AtomEventBusRegistry extends AtomEventRegistrable<Object> {
-    void registerBus(@NonNull AtomEventBus<?> bus, @Nullable Bindable registerer);
-}
